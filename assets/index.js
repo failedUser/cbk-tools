3b0b90ce0532363cd1f33743a1c08e65840bd437
changsu <yyasyya@163.com>
2019-12-16 16:47:54 +0800

    0.0.1-beta.2

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+export { github } from './github.png';
+export { border } from './border.png';
+export { sm } from './sm.jpg';
+export { yiquan } from './yiquan.jpeg';
+export { yiquan2 } from './yiquan2.jpeg';
